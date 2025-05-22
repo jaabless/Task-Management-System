64cc96d6bd4123d503e3cdb9d90acdae04150fbb
jaabless <jefferyasamoahbless@gmail.com>
2025-05-22 12:32:36 +0000

    Add main

---
+++ src/main/java/com/novatech/taskmanager/Main.java
@@ -0,0 +1,23 @@
+package com.novatech.taskmanager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    }
+    public static int addNumbers(int a, int b){
+        return a+b;
+    }
+
+    Set newSet;
+//    Set newObj = new ArrayList<>();
+
+
+}
